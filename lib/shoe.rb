c5ab095fcf08fa845225e45d378e9ccd2e717181
Makeda P Mckune <makedamckune@gmail.com>
2019-11-14 14:19:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,13 @@
 class Shoe 
-  def initialize(brand,)
+  def initialize(brand)
+    @brand = brand 
+  end
   
+  def brand=(brand)
+    @brand = brand 
+  end
+  
+  def brand 
+    @brand 
   end 
 end 
